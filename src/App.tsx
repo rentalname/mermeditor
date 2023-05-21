8189d7d7edf291cee532dc3dfc5c37cf8a7d89ec
rentalname <agenda21st@gmail.com>
2023-05-22 01:17:27 +0900

    :construction:

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,57 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
-import mmd from "mermaid";
+import mermaid from "mermaid";
 import dedent from 'ts-dedent'
 import "./App.css";
 
-const api = mmd.mermaidAPI
+const api = mermaid.mermaidAPI
 
 api.initialize({ startOnLoad: false })
 
 function App() {
+  const [error, setError] = useState({
+    parseError: false,
+  })
   const [svg, setSvg] = useState("")
+  const source = useRef<HTMLTextAreaElement>(null)
 
-  const reload = async (text: string) => {
-    api.render('theGraph', text).then((result) => {
-      setSvg(result.svg)
-    })
+  const render = async (text: string) => {
+    const valid = await api.parse(text, { suppressErrors: true })
+
+    if (valid) {
+      const { svg } = await api.render('theGraph', text)
+      setSvg(svg)
+      setError((error) => ({ ...error, parseError: false }))
+    } else {
+      setError((error) => ({ ...error, parseError: true }))
+    }
   }
+
+
+  const rerender = async () => {
+    if (!source.current) return
+
+    const code = source.current.value
+
+    render(code)
+  }
+
+  const save = async () => {
+    if (!source.current) return
+
+    const code = source.current.value
+
+    invoke('export', { code })
+  }
+
+  console.log(error)
 
   return (
     <div className="container">
       <h1>Offline Mermaid Editor</h1>
 
       <div className="row">
-        <pre className="codeArea" contentEditable onChange={(e) => reload(e.currentTarget.innerText)}>
+        <textarea cols={80} rows={30} className="codeArea" ref={source} onChange={rerender}>
           {dedent(`
             sequenceDiagram
               A->> B: Query
@@ -31,7 +60,13 @@ function App() {
               C->> B: Response
               B->> A: Forward response
           `)}
-        </pre>
+        </textarea>
+      </div>
+      <div className="row">
+        <button onClick={rerender}><span>render</span>{error.parseError ? '⚠️' : null}</button>
+        <button onClick={save}><span>save</span>{error.parseError ? '⚠️' : null}</button>
+      </div>
+      <div className="row">
         <div className="renderArea" dangerouslySetInnerHTML={{ __html: svg }}></div>
       </div>
     </div>
