20f608f9e33463c7b1d28264f713c1dfe6e35156
rentalname <agenda21st@gmail.com>
2023-05-22 01:17:35 +0900

    :construction:

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,33 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+use std::fs::File;
+use std::process::Command;
+use std::io::{Write};
+use std::env;
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn export(code: String) {
+    let dir = env::temp_dir();
+    let f_path = dir.as_path().join("a.md");
+    let mut file = File::create(f_path).expect("msg");
+    writeln!(file, "```mermaid\n{}\n```", code).expect("msg");
+
+    let xx = dir.as_path().join("a.md");
+
+    let result =
+      Command::new("yarn")
+        .arg("mmdc")
+        .args(["--input", xx.to_str().expect("msg")])
+        .output()
+        .expect("errrrrrooor");
+    let y = String::from_utf8(result.stdout).unwrap();
+    println!("{}", "::TODO::");
+    println!("{}", y);
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![export])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
