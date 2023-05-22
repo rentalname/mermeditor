3b7836e70f0e2853071f13c24d93c4993b601f08
rentalname <agenda21st@gmail.com>
2023-05-22 09:16:10 +0900

    :construction:

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,21 +3,25 @@
 
 use std::fs::File;
 use std::process::Command;
-use std::io::{Write};
+use std::io::Write;
 use std::env;
+
+use tauri::api::file;
 #[tauri::command]
 fn export(code: String) {
+    let filename = "a.mmd";
     let dir = env::temp_dir();
-    let f_path = dir.as_path().join("a.md");
+    let f_path = dir.as_path().join(filename);
     let mut file = File::create(f_path).expect("msg");
-    writeln!(file, "```mermaid\n{}\n```", code).expect("msg");
+    writeln!(file, "{}", code).expect("msg");
 
-    let xx = dir.as_path().join("a.md");
+    let f_path = dir.as_path().join(filename);
 
     let result =
       Command::new("yarn")
         .arg("mmdc")
-        .args(["--input", xx.to_str().expect("msg")])
+        .args(["--input", f_path.to_str().expect("msg")])
+        .args(["-e", "png"])
         .output()
         .expect("errrrrrooor");
     let y = String::from_utf8(result.stdout).unwrap();
